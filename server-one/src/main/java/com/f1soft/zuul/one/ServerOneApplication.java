e862b9674ce34a2650cec051abbb83d70eeb4621
Rashim Dhaubanjar <rashim.dhaubanjar@f1soft.com>
2021-01-12 20:35:28 +0545

    refine

--- server-one/src/main/java/com/f1soft/zuul/one/ServerOneApplication.java
+++ server-one/src/main/java/com/f1soft/zuul/one/ServerOneApplication.java
@@ -1,18 +1,12 @@
 package com.f1soft.zuul.one;
 
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.hazelcast.HazelcastAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.util.Map;
 
 /**
  * @author Rashim Dhaubanjar
@@ -21,20 +15,11 @@ import java.util.Map;
 @ComponentScan(basePackages = {"com.f1soft"})
 @EnableJpaRepositories(basePackages = {"com.f1soft"})
 @EnableCaching
-@SpringBootApplication(exclude = HazelcastAutoConfiguration.class)
-public class ServerOneApplication extends SpringBootServletInitializer implements CommandLineRunner {
+@SpringBootApplication
+public class ServerOneApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerOneApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        HazelcastInstance hz = Hazelcast.getOrCreateHazelcastInstance();
-        Map<String, String> map = hz.getMap("map");
-        map.put("1", "Tokyo");
-        map.put("2", "Paris");
-        map.put("3", "New York");
-        System.out.println("Finished loading map");
-    }
 }
--- server-two/src/main/java/com/f1soft/zuul/proxy/two/ServerTwoApplication.java
+++ server-two/src/main/java/com/f1soft/zuul/proxy/two/ServerTwoApplication.java
@@ -1,19 +1,12 @@
 package com.f1soft.zuul.proxy.two;
 
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.hazelcast.HazelcastAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.util.Map;
 
 /**
  * @author Rashim Dhaubanjar
@@ -22,7 +15,7 @@ import java.util.Map;
 @ComponentScan(basePackages = {"com.f1soft"})
 @EnableJpaRepositories(basePackages = {"com.f1soft"})
 @EnableCaching
-@SpringBootApplication(exclude = HazelcastAutoConfiguration.class)
+@SpringBootApplication
 public class ServerTwoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
